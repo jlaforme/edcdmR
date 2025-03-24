04fb649d09f80884fa70cafbfaeb2a2060b1b5c0
jlaforme <144736621+jlaforme@users.noreply.github.com>
2025-03-24 16:43:52 -0400

    add NA to allowed forms

--- R/REDCap_filter.R
+++ R/REDCap_filter.R
@@ -3,7 +3,7 @@
 #' @description Reruens a filtered dataset based on forms and variables arguments.
 #'
 #' @param data Dataset to filter
-#' @param data_dictionary Data dictionary
+#' @param dictionary Data dictionary
 #' @param id_variable Name of the identifier variable
 #' @param forms Forms to include; defaults to all
 #' @param variables Variables to include; defaults to all
@@ -11,18 +11,20 @@
 #' @returns A filtered dataframe
 #' @export
 #'
-#' @examples REDCap_filter(data = REDCap_data, data_dictionary = REDCap_dd, id_variable = "record_id")
-#' @examples REDCap_filter(data = REDCap_data, data_dictionary = REDCap_dd, id_variable = "record_id", forms = c("form1", "form2", "..."), variables = c("var1", "var2", "..."))
+#' @examples REDCap_filter(data = REDCap_data, dictionary = REDCap_dd, id_variable = "record_id")
+#' @examples REDCap_filter(data = REDCap_data, dictionary = REDCap_dd, id_variable = "record_id", forms = c("form1", "form2", "..."), variables = c("var1", "var2", "..."))
 
-REDCap_filter <- function(data, data_dictionary, id_variable, forms = NULL, variables = NULL) {
+REDCap_filter <- function(data, dictionary, id_variable, forms = NULL, variables = NULL) {
   library(dplyr)
   library(xml2)
   library(tidyr)
   library(stringr)
-
+data <- project$data
+dictionary <- project$dictionary
+variables = c("iec_q005", "pld_q001", "pld_q003___0", "pld_q003___1", "pld_q003___2", "pld_q003___3", "pld_q003___4", "pld_q003___5", "pld_q003___6", "pld_q003___7", "pld_q003____7777")
   # Clean data
   # Data dictionnary
-  data_dictionary <- data_dictionary %>%
+  dictionary <- dictionary %>%
     mutate(across(where(is.character), ~ na_if(., ""))) %>%
     rename_with(~ ifelse(str_detect(., regex("var|field name|field_name", ignore_case = TRUE)), "var_name", .)) %>%
     rename_with(~ ifelse(str_detect(., regex("form", ignore_case = TRUE)), "form_name", .)) %>%
@@ -42,16 +44,16 @@ REDCap_filter <- function(data, data_dictionary, id_variable, forms = NULL, vari
 
   metadata_columns <- names(data)[!sapply(names(data), function(x) {
     base_var <- strsplit(x, "___")[[1]][1]
-    base_var %in% data_dictionary$var_name | grepl("_complete$", x)
+    base_var %in% dictionary$var_name | grepl("_complete$", x)
   })]
 
 
   # Define the id_variable
-  id_variable <- data_dictionary$var_name[1]
+  id_variable <- dictionary$var_name[1]
 
   # Fill the forms if empty
   if (is.null(forms)) {
-    forms = unique(data_dictionary$form_name)
+    forms <- c(NA, unique(dictionary$form_name))
   }
 
   if (is.null(variables) || length(variables) == 0 || any(variables %in% c("All", "all"))) {
@@ -62,7 +64,7 @@ REDCap_filter <- function(data, data_dictionary, id_variable, forms = NULL, vari
   # Data
   data_filtered <- data %>%
     mutate(across(where(is.character), ~na_if(., ""))) %>%
-    select({{id_variable}}, all_of(metadata_columns), starts_with(data_dictionary$var_name[data_dictionary$form_name %in% forms])) %>%
+    select({{id_variable}}, all_of(metadata_columns), starts_with(dictionary$var_name[dictionary$form_name %in% forms])) %>%
     filter(redcap_repeat_instrument %in% forms) %>%
     filter(!if_all(-c({{id_variable}}, all_of(metadata_columns)), is.na)) %>%
     select({{id_variable}}, all_of(metadata_columns), any_of(variables))
